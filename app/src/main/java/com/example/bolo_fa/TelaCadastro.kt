4ed3b9d55afebcd12eae085a042a7c627c580f43
Rafael Martins Nascimento <rafael.mnascimento@totvs.com.br>
2024-11-04 16:04:41 -0300

    Criando validação na tela de cadastrado

--- app/src/main/java/com/example/bolo_fa/TelaCadastro.kt
+++ app/src/main/java/com/example/bolo_fa/TelaCadastro.kt
@@ -3,6 +3,7 @@ package com.example.bolo_fa
 import Controller.AuthenticationController
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -29,9 +30,26 @@ class TelaCadastro : AppCompatActivity() {
         }
 
         binding.btnCadastrarUsuario.setOnClickListener{
+            val nome = binding.etNomeCadastro.text.toString()
             val email = binding.etEmailCadastro.text.toString()
             val senha = binding.etSenhaCadastro.text.toString()
             val confirmarSenha = binding.etConfirmarSenha.text.toString()
+
+            if(nome.isEmpty() || email.isEmpty() || senha.isEmpty() || confirmarSenha.isEmpty()){
+                Toast.makeText(this, "Existem campos que ainda não foram preenchido", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener;
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(this, "E-mail inválido", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(senha != confirmarSenha){
+                Toast.makeText(this, "Os valores nos campos Nova Senha e Confirmar Senha não correspondem. Insira a senha desejada em ambos os campos.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener;
+            }
+
 
             controller = AuthenticationController()
 
