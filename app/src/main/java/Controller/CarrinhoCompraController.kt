044a9ab6359e406846c344de1deb5ebae8551b6e
Rafael Martins Nascimento <rafael.mnascimento@totvs.com.br>
2024-11-22 14:40:17 -0300

    Funcionalidade de:                          Adicionar uma quantidade                  Remover uma quantidade                    Remover item da quantidade

--- app/src/main/java/Controller/CarrinhoCompraController.kt
+++ app/src/main/java/Controller/CarrinhoCompraController.kt
@@ -1,7 +1,12 @@
 package Controller
 import Model.CarrinhoCompra
+import android.widget.Toast
+import com.example.bolo_fa.carrinho_compras
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+
+
 
 class CarrinhoCompraController {
 
@@ -20,5 +25,70 @@ class CarrinhoCompraController {
 
     fun listarPorEmailUsuario(email: String?): Query{
         return db.collection("CarrinhoCompras").whereEqualTo("email", email)
+    }
+
+    fun adicionarMaisUmQuantidade(produto: String?, onResult: (Boolean, String?) -> Unit) {
+        val colecao = db.collection("CarrinhoCompras")
+
+        colecao.whereEqualTo("produto", produto).get()
+            .addOnSuccessListener { result ->
+                if (!result.isEmpty) {
+                    val document = result.documents.first()
+                    val carrinhoId = document.id
+
+                    colecao.document(carrinhoId).update("quantidade", FieldValue.increment(1))
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                onResult(true, null)
+                            } else {
+                                onResult(false, task.exception?.message)
+                            }
+                        }
+                } else {
+                    onResult(false, "Produto não encontrado no carrinho")
+                }
+            }
+    }
+
+    fun removerUmnaQuantidade(produto: String?, onResult: (Boolean, String?) -> Unit){
+        val colecao = db.collection("CarrinhoCompras")
+
+        colecao.whereEqualTo("produto", produto).get()
+            .addOnSuccessListener { result ->
+                if (!result.isEmpty) {
+                    val document = result.documents.first()
+                    val carrinhoId = document.id
+
+                    colecao.document(carrinhoId).update("quantidade", FieldValue.increment(-1))
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                onResult(true, null)
+                            } else {
+                                onResult(false, task.exception?.message)
+                            }
+                        }
+                } else {
+                    onResult(false, "Produto não encontrado no carrinho")
+                }
+            }
+    }
+
+    fun deletarProdutoCarrinho(produto: String?, onResult: (Boolean, String?) -> Unit) {
+        if (produto != null) {
+            db.collection("CarrinhoCompras").whereEqualTo("produto", produto).get()
+                .addOnSuccessListener { result ->
+                    if (!result.isEmpty) {
+                        val document = result.documents.first()
+
+                        document.reference.delete().addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                onResult(true, null)
+                            } else {
+                                onResult(false, "Erro ao remover o item do carrinho")
+                            }
+                        }
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/Controller/ResumoCardapioAdapter.kt
+++ app/src/main/java/Controller/ResumoCardapioAdapter.kt
@@ -3,6 +3,7 @@ package Controller
 import Model.CarrinhoCompra
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bolo_fa.databinding.ResumoCardapioAdapterBinding
 
@@ -25,6 +26,47 @@ class ResumoCardapioAdapter(private var lista: MutableList<CarrinhoCompra>) : Re
             val item = lista[position]
             holder.binding.produto.text = item.produto
             holder.binding.productPrice.text = item.preco
+            holder.binding.productQuantity.text = "Quantidade: " + item.quantidade
+
+            holder.binding.addButton.setOnClickListener{
+                val carrinhoCompraController = CarrinhoCompraController()
+
+                carrinhoCompraController.adicionarMaisUmQuantidade(item.produto){ sucesso, erro ->
+                    if(sucesso){
+                        Toast.makeText(holder.itemView.context, "Adicionado mais uma quantidade do produto", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(holder.itemView.context, "Erro ao adicionar o item ", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            holder.binding.removeButton.setOnClickListener{
+                val carrinhoCompraController = CarrinhoCompraController()
+
+                carrinhoCompraController.removerUmnaQuantidade(item.produto){sucesso, erro ->
+                    if(sucesso){
+                        Toast.makeText(holder.itemView.context, "Removido 1 quantidade desse produto", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(holder.itemView.context, "Falha ao remover quantidade desse produto", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            holder.binding.deleteButton.setOnClickListener{
+                val carrinhoController = CarrinhoCompraController()
+
+                carrinhoController.deletarProdutoCarrinho(item.produto){sucesso, erro ->
+                    if(sucesso){
+                        Toast.makeText(holder.itemView.context, "Item removido com sucesso", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(holder.itemView.context, "Erro ao remover", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                lista.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, lista.size)
+            }
+
         }
     }
 
